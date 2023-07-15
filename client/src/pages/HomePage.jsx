15bd3ad09614e83071fdad4a760e20f24c8465a1
Nadine Benarbia <nadinebenarbia@gmail.com>
2023-07-15 16:06:11 +0100

    login/homepage css

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import HeroSection from '../Components/HeroSection/HeroSection'
+import AboutUs from '../Components/AboutUs/AboutUs'
+import MoreInfo from '../Components/moreInfo/MoreInfo'
+import Collection from '../Components/collection/collection'
+import Faq from '../Components/FAQ/Faq'
+import Quality from '../Components/quality collection/Quality'
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection/>
+      <AboutUs/>
+      <MoreInfo/>
+      <Collection/>
+      <span> display some of the product here </span>
+      <span> display some trending products</span>
+      <span> upcoming products</span>
+      <Faq/>
+      <Quality/>
     </div>
   )
 }
