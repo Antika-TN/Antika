fc942550e09a1637c23ae52013efd306e9cd38e9
fares boutriga <boutrigafares@gmail.com>
2023-07-16 09:56:38 +0100

    16/07/9:56

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -3,14 +3,11 @@ import React from 'react'
 const AdminPage = () => {
   return (
     <div>
-      
     <div className='cards'>
     
     <Cards/>
     
     </div>
-    <h1 className='title'>Top Market Statistics</h1>
-    <h2 className='underTitle'>The NFTs on_____ ,ranked by volume,floor price and other statistics</h2>
     <div>
      
       <DashboardTable sellersname={sellersname} />
